211b46fc0965cb91a9b67324a087a133d82e95b3
5730593821-SP <supakritboom@hotmail.com>
2015-08-21 10:59:16 +0700

    I've just changed something. you know?

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "love you so much my java";
 	}
 	
 	public static int[] bubbleSort(int[] array){
